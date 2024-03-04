c3b9523b0968ede29d7653dac403f92d59657291
Adrian <adriansandu9090@gmail.com>
2024-03-04 19:37:52 +0000

    added groups to RegEX

--- script.js
+++ script.js
@@ -11,12 +11,22 @@ const inputElement = document.getElementById("equation")
 const outputElement = document.getElementById("results")
 const form = document.getElementById("equation-form")
 
+const MULTIPLY_DIVIDE_REGEX = /(?<operand1>\d+)\s*(?<operation>[\/\*])\s*(?<operand2>\d+)/
+
 form.addEventListener("submit", e => {
     e.preventDefault()
 
     const result = parse(inputElement.value)
+    outputElement.textContent = result
 })
 
 function parse(equation) {
+    return equation
+}
+
+function handleMath({ operand1, operand2, operation }) {
 
 }
+
+const equation = "2+3*4234/723"
+console.log(equation.match(MULTIPLY_DIVIDE_REGEX))
