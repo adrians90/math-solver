ecf04ede10be68e2196f3b6925063417bd1a72a7
Adrian <adriansandu9090@gmail.com>
2024-03-04 19:25:42 +0000

    selected form, results and equation elements. added event listener to the form

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// function parse(equation) {
+//     const step = getNextStep()
+//     const result = solve(step)
+//     replaceNextStep(equation, step, result)
+
+// }
+
+const inputElement = document.getElementById("equation")
+const outputElement = document.getElementById("results")
+const form = document.getElementById("equation-form")
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const result = parse(inputElement.value)
+})
+
+function parse(equation) {
+
+}
